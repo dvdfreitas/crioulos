f277da2c17b643232d3f5a182234209c3eaa906d
Rafael Vieira <rafinhagv06@hotmail.com>
2024-04-27 16:41:13 +0100

    Adicionei informação e mais línguas crioulas

--- resources/views/components/navigation-menu-guest.blade.php
+++ resources/views/components/navigation-menu-guest.blade.php
@@ -1,8 +1,8 @@
-<nav>
-    <div class="flex">
-        <a href="{{ route('welcome') }}"><div class="m-3 border-2">Homepage</div></a>
-        <a href="{{ route('language_codes.index') }}"><div class="m-3 border-2">Línguas</div></a>
-        <a href="{{ route('references.index') }}"><div class="m-3 border-2">Referências</div></a>
-        <a href="{{ route('people.index') }}"><div class="m-3 border-2">Pessoas</div></a>
+<nav class="bg-white">
+    <div class="flex shadow-sm p-4 gap-3">
+        <a href="{{ route('welcome') }}"><div class="p-2 border-2">Homepage</div></a>
+        <a href="{{ route('language_codes.index') }}"><div class="p-2 border-2">Línguas</div></a>
+        <a href="{{ route('references.index') }}"><div class="p-2 border-2">Referências</div></a>
+        <a href="{{ route('people.index') }}"><div class="p-2 border-2">Pessoas</div></a>
     </div>
 </nav>
--- resources/views/language_codes/index.blade.php
+++ resources/views/language_codes/index.blade.php
@@ -1,9 +1,13 @@
 <x-guestLayout>
-    @foreach ($language_codes as $language_code)
-        <div class="bg-white shadow overflow-hidden sm:rounded-lg mb-4">
+    <h1 class="text-3xl text-center font-bold m-6">Línguas</h1>
+    <div class="grid grid-cols-4 gap-4 m-6">
+        @foreach ($language_codes as $language_code)
+        <div class="bg-white shadow-md overflow-hidden sm:rounded-lg">
             <div class="px-4 py-5 sm:px-6">
-                <h3 class="text-lg font-medium leading-6 text-gray-900">{{ $language_code->name }}</h3>
+                <h3 class="text-xl font-semibold mb-4 leading-6 text-gray-900">{{ $language_code->name }}</h3>
+                <p class="text-md font-medium leading-6 text-gray-900">{{ $language_code->description }}</p>
             </div>
         </div>
-    @endforeach    
+        @endforeach
+    </div>
 </x-guestLayout>

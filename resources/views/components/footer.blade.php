f79ff6c24ccdafbe8dbb3a994937bca4689ae05e
Rafael Vieira <rafinhagv06@hotmail.com>
2024-05-06 09:11:30 +0100

    footer adicionado e design do tradutor alterado

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,64 @@
-<div class="h-48 bg-black">
-    <p class="text-white">Inserir o footer</p>
-</div>
+<footer class="relative bg-white pt-8 pb-6">
+    <div class="container mx-auto px-4">
+        <div class="flex flex-wrap text-left lg:text-left">
+            <div class="w-full lg:w-6/12 px-4">
+                <h4 class="text-3xl fonat-semibold text-amber-700 mb-2">Todos são bem-vindos a participar e colaborar!</h4>
+                <h5 class="text-lg mt-0 mb-2 text-gray-600">
+                    O projeto Crioulos é open source, o que significa que está aberto a contribuições de qualquer pessoa interessada em impulsionar o seu crescimento e desenvolvimento.
+                </h5>
+                <div class="mt-6 lg:mb-0 mb-6">
+                    <!-- GitHub -->
+                    <a href="https://github.com/dvdfreitas/crioulos">
+                        <button class="bg-white text-amber-700 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button"><i class="fa-brands fa-github fa-xl"></i></button>
+                    </a>
+                </div>
+            </div>
+            <div class="w-full lg:w-6/12 px-4">
+                <div class="flex flex-wrap items-top mb-6">
+                    <div class="w-full lg:w-4/12 px-4 ml-auto">
+                        <span class="block uppercase text-amber-700 text-sm font-semibold mb-2">Useful Links</span>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a class="text-gray-600 hover:text-gray-800 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/presentation?ref=njs-profile">About Us</a>
+                            </li>
+                            <li>
+                                <a class="text-gray-600 hover:text-gray-800 font-semibold block pb-2 text-sm" href="https://blog.creative-tim.com?ref=njs-profile">Blog</a>
+                            </li>
+                            <li>
+                                <a class="text-gray-600 hover:text-gray-800 font-semibold block pb-2 text-sm" href="https://www.github.com/creativetimofficial?ref=njs-profile">Github</a>
+                            </li>
+                            <li>
+                                <a class="text-gray-600 hover:text-gray-800 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/bootstrap-themes/free?ref=njs-profile">Free Products</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="w-full lg:w-4/12 px-4">
+                        <span class="block uppercase text-amber-700 text-sm font-semibold mb-2">Other Resources</span>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a class="text-gray-600 hover:text-gray-800 font-semibold block pb-2 text-sm" href="https://github.com/creativetimofficial/notus-js/blob/main/LICENSE.md?ref=njs-profile">MIT License</a>
+                            </li>
+                            <li>
+                                <a class="text-gray-600 hover:text-gray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/terms?ref=njs-profile">Terms &amp; Conditions</a>
+                            </li>
+                            <li>
+                                <a class="text-gray-600 hover:text-gray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/privacy?ref=njs-profile">Privacy Policy</a>
+                            </li>
+                            <li>
+                                <a class="text-gray-600 hover:text-gray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/contact-us?ref=njs-profile">Contact Us</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr class="my-6 border-gray-300">
+        <div class="flex flex-wrap items-center md:justify-between justify-center">
+            <div class="w-full md:w-4/12 px-4 mx-auto text-center">
+                <div class="text-sm text-gray-500 font-semibold py-1">
+                    <!-- Copyright © -->
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
